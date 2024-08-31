df25e9f3176c9440c6793a06986cdf5848faf827
BaranovMV-IT <mishasamarabaranov2002@mail.ru>
2024-08-31 12:01:48 +0400

    Base setup

---
+++ src/pages/CatalogPage.vue
@@ -0,0 +1,15 @@
+<template>
+
+</template>
+
+<script setup lang="ts">
+import { defineComponent, ref } from 'vue';
+
+defineComponent({
+  name: 'CatalogPage',
+});
+</script>
+
+<style lang="scss" scoped>
+
+</style>
